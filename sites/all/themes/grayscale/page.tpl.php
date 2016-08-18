889415f1fad41b3d5999d8449a02d6523243f281
Ben Ronda <bronda95@comcast.net>
2016-08-18 11:47:33 -0700

    add files to github

--- sites/all/themes/grayscale/page.tpl.php
+++ sites/all/themes/grayscale/page.tpl.php
@@ -73,43 +73,53 @@
  */
 ?>
 
-  <div id="page-wrapper">
-    <div id="page">
-      <div id="header"><div class="section clearfix">
-          <?php if ($logo): ?>
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-            </a>
-          <?php endif; ?>
-
-          <?php if ($site_name || $site_slogan): ?>
-            <div id="name-and-slogan">
-              <?php if ($site_name): ?>
-                <?php if ($title): ?>
-                  <div id="site-name"><strong>
-                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span>
-                  </strong></div>
-                <?php else: /* Use h1 when the content title is empty */ ?>
-                  <h1 id="site-name">
-                    <span><?php print $site_name; ?></span>
-                  </h1>
-                <?php endif; ?>
+      <div id="header">
+          <div id="about-me">
+              <p>
+                  hello
+              </p>
+          </div>
+          <!-- <div class="section clearfix">
+              <?php if ($logo): ?>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+                  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                </a>
               <?php endif; ?>
 
-              <?php if ($site_slogan): ?>
-                <div id="site-slogan"><?php print $site_slogan; ?></div>
+              <?php if ($site_name || $site_slogan): ?>
+                <div id="name-and-slogan">
+                  <?php if ($site_name): ?>
+                    <?php if ($title): ?>
+                      <div id="site-name"><strong>
+                        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span>
+                      </strong></div>
+                    <?php else: /* Use h1 when the content title is empty */ ?>
+                      <h1 id="site-name">
+                        <span><?php print $site_name; ?></span>
+                      </h1>
+                    <?php endif; ?>
+                  <?php endif; ?>
+
+                  <?php if ($site_slogan): ?>
+                    <div id="site-slogan"><?php print $site_slogan; ?></div>
+                  <?php endif; ?>
+                </div> <!-- /#name-and-slogan -->
               <?php endif; ?>
-            </div> <!-- /#name-and-slogan -->
-          <?php endif; ?>
 
-          <?php print render($page['header']); ?>
+              <?php print render($page['header']); ?>
 
-        </div></div> <!-- /.section, /#header -->
+          </div>
+
+
+        <!-- /.section, /#header -->
+
+
         <div id = "main_content">
             <?php if ($main_menu || $secondary_menu): ?>
+                <!-- <?php var_dump($main_menu);?> -->
               <div id="navigation"><div class="section">
-                <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-                <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+                <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
+                <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
               </div></div> <!-- /.section, /#navigation -->
             <?php endif; ?>
 
@@ -149,9 +159,8 @@
             </div></div> <!-- /#main, /#main-wrapper -->
         </div>
 
-        <div id="footer"><div class="section">
-          <?php print render($page['footer']); ?>
-        </div></div> <!-- /.section, /#footer -->
-
-      </div>
-    </div> <!-- /#page, /#page-wrapper -->
+        <div id="footer">
+            <div class="section">
+                <?php print render($page['footer']); ?>
+            </div>
+        </div> <!-- /.section, /#footer -->
